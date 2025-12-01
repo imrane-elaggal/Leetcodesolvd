2e25281393427f5474c9cb51793fed0284dc483f
IMRANE ELAGGAL <131166452+imrane-elaggal@users.noreply.github.com>
2025-12-01 16:19:33 +0100

    Time: 5 ms (70.59%), Space: 45.9 MB (30.14%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+   public boolean isPalindrome(int n) {
+        int r, sum = 0, temp;
+
+        temp = n;
+        while (n > 0) {
+            r=n%10;
+            sum = (sum * 10) + r ;
+            n = n / 10;
+        }
+        if (temp == sum)
+            return true;
+        else
+            return false;
+
+
+    }
+}
